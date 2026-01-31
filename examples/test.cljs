209e5da748637cb9c7d79cda3da86ca8918832b8
Ravindra R. Jaju <jaju@msync.org>
2026-01-31 22:53:55 +0530

    Added a couple of example files - js and cljs

---
+++ examples/test.cljs
@@ -0,0 +1,6 @@
+;; Eval
+(.-title js/document)
+
+;; Define a function and then execute it
+(defn title [] (.-title js/document))
+(title)
---
+++ examples/test.js
@@ -0,0 +1,8 @@
+// Arithmetic expression
+1 + 1;
+
+// Attribute access
+document.title
+
+// Action
+document.location.href = "https://mail.google.com/"
